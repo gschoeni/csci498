a193616252245af3d152fa05180f17d06b457a2c
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-08-31 14:52:47 -0600

    implemented a simple event listener for the save button

--- LunchList/src/csci498/gregschoeninger/lunchlist/LunchList.java
+++ LunchList/src/csci498/gregschoeninger/lunchlist/LunchList.java
@@ -1,15 +1,22 @@
 package csci498.gregschoeninger.lunchlist;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LunchList extends Activity {
+	
+	Restaurant r=new Restaurant();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
+        Button save = (Button)findViewById(R.id.save); 
+        save.setOnClickListener(onSave);
     }
 
     @Override
@@ -17,4 +24,15 @@ public class LunchList extends Activity {
         getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
         return true;
     }
+    
+    private View.OnClickListener onSave = new View.OnClickListener() {
+    	
+    	public void onClick(View v) {
+	    	EditText name = (EditText)findViewById(R.id.name); 
+	    	EditText address = (EditText)findViewById(R.id.addr);
+	    	r.setName(name.getText().toString());
+	    	r.setAddress(address.getText().toString()); 
+    	}
+    	
+    };
 }
