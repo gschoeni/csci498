1633b032e8eaab6c8e6a82d7f22b292a2961a62b
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-09-02 13:13:44 -0600

    created radio buttons programatically.. really prefer to create them in xml but oh well!

--- LunchList/src/csci498/gregschoeninger/lunchlist/LunchList.java
+++ LunchList/src/csci498/gregschoeninger/lunchlist/LunchList.java
@@ -1,24 +1,29 @@
 package csci498.gregschoeninger.lunchlist;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class LunchList extends Activity {
 	
-	Restaurant restaurant = new Restaurant();
+	private Restaurant restaurant = new Restaurant();
+	private RadioGroup types;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
+        
+        addTypesRadioGroup();
+        
         Button save = (Button)findViewById(R.id.save); 
         save.setOnClickListener(onSave);
-        
     }
 
     @Override
@@ -27,18 +32,36 @@ public class LunchList extends Activity {
         return true;
     }
     
+    private void addTypesRadioGroup(){
+    	types = (RadioGroup)findViewById(R.id.types);
+        
+        RadioButton sit_down = new RadioButton(this);
+        sit_down.setText(R.string.sit_down_type);
+        sit_down.setId(R.id.sit_down);
+        types.addView(sit_down);
+        
+        RadioButton takeout = new RadioButton(this);
+        takeout.setText(R.string.takeout_type);
+        takeout.setId(R.id.take_out);
+        types.addView(takeout);
+        
+        RadioButton delivery = new RadioButton(this);
+        delivery.setText(R.string.delivery_type);
+        delivery.setId(R.id.delivery);
+        types.addView(delivery);
+        
+        types.check(delivery.getId());
+    }
+    
     private View.OnClickListener onSave = new View.OnClickListener() {
     	
     	public void onClick(View v) {
-    		System.out.println("Clicked");
 	    	EditText name = (EditText)findViewById(R.id.name); 
 	    	EditText address = (EditText)findViewById(R.id.addr);
 	    	restaurant.setName(name.getText().toString());
 	    	restaurant.setAddress(address.getText().toString()); 
 	    	
-	    	RadioGroup types = (RadioGroup)findViewById(R.id.types);
 	    	setRestaurantType(types);
-	    	
 	    }
     	
     	public void setRestaurantType(RadioGroup types){
