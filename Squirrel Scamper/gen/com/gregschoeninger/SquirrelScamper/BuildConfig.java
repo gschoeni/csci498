3ff04cb2bfca87d893d2fe58a59209bf62990b7a
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-09-20 22:28:46 -0600

    Adding base files for final project, Squirrel Scamper

---
+++ Squirrel Scamper/gen/com/gregschoeninger/SquirrelScamper/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.gregschoeninger.SquirrelScamper;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
