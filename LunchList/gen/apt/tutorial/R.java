ef237b9a57d15be6859be90b6c8227b221bb3262
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-09-25 19:38:51 -0600

    added xml files for preference settings

--- LunchList/gen/apt/tutorial/R.java
+++ LunchList/gen/apt/tutorial/R.java
@@ -8,6 +8,10 @@
 package apt.tutorial;
 
 public final class R {
+    public static final class array {
+        public static final int sort_clauses=0x7f050001;
+        public static final int sort_names=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -23,18 +27,18 @@ public final class R {
         public static final int toast=0x7f020009;
     }
     public static final class id {
-        public static final int add=0x7f07000b;
-        public static final int address=0x7f070001;
-        public static final int delivery=0x7f070005;
-        public static final int icon=0x7f070008;
-        public static final int menu_settings=0x7f07000a;
-        public static final int name=0x7f070000;
-        public static final int notes=0x7f070006;
-        public static final int save=0x7f070007;
-        public static final int sit_down=0x7f070004;
-        public static final int take_out=0x7f070003;
-        public static final int title=0x7f070009;
-        public static final int types=0x7f070002;
+        public static final int add=0x7f09000b;
+        public static final int address=0x7f090001;
+        public static final int delivery=0x7f090005;
+        public static final int icon=0x7f090008;
+        public static final int menu_settings=0x7f09000a;
+        public static final int name=0x7f090000;
+        public static final int notes=0x7f090006;
+        public static final int save=0x7f090007;
+        public static final int sit_down=0x7f090004;
+        public static final int take_out=0x7f090003;
+        public static final int title=0x7f090009;
+        public static final int types=0x7f090002;
     }
     public static final class layout {
         public static final int activity_lunch_list=0x7f030000;
@@ -42,23 +46,26 @@ public final class R {
         public static final int row=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_lunch_list=0x7f060000;
-        public static final int option=0x7f060001;
+        public static final int activity_lunch_list=0x7f080000;
+        public static final int option=0x7f080001;
     }
     public static final class string {
-        public static final int address_label=0x7f040005;
-        public static final int app_name=0x7f040000;
-        public static final int delivery=0x7f040003;
-        public static final int menu_settings=0x7f040009;
-        public static final int name_label=0x7f040004;
-        public static final int notes_label=0x7f040007;
-        public static final int save_button=0x7f040008;
-        public static final int sit_down=0x7f040001;
-        public static final int take_out=0x7f040002;
-        public static final int title_activity_lunch_list=0x7f04000a;
-        public static final int type_label=0x7f040006;
+        public static final int address_label=0x7f060005;
+        public static final int app_name=0x7f060000;
+        public static final int delivery=0x7f060003;
+        public static final int menu_settings=0x7f060009;
+        public static final int name_label=0x7f060004;
+        public static final int notes_label=0x7f060007;
+        public static final int save_button=0x7f060008;
+        public static final int sit_down=0x7f060001;
+        public static final int take_out=0x7f060002;
+        public static final int title_activity_lunch_list=0x7f06000a;
+        public static final int type_label=0x7f060006;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f070000;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
