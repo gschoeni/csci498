39a03ce113307269a533b05c2976dcde91bfee72
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-09-30 21:42:01 -0600

    FeedActivity class is finished using the RSSReader, get a list of the titles of the articles from the rss feed, snazzy

--- LunchList/src/apt/tutorial/FeedActivity.java
+++ LunchList/src/apt/tutorial/FeedActivity.java
@@ -1,18 +1,24 @@
 package apt.tutorial;
 
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
 import org.mcsoxford.rss.RSSReader;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class FeedActivity extends ListActivity {
+	
+	private InstanceState state;
+	public static final String FEED_URL = "apt.tutorial.FEED_URL";
 	
 	private static class FeedTask extends AsyncTask<String, Void, RSSFeed>{
 		private Exception e;
@@ -25,6 +31,10 @@ public class FeedActivity extends ListActivity {
 		
 		void attach(FeedActivity activity){
 			this.activity = activity;
+		}
+		
+		void detach() {
+			this.activity = null;
 		}
 		
 		@Override
@@ -43,7 +53,7 @@ public class FeedActivity extends ListActivity {
 		@Override
 		public void onPostExecute(RSSFeed feed){
 			if(e == null){
-				//activity.setFeed(feed);
+				activity.setFeed(feed);
 			} else {
 				Log.e("LunchList", "Exception parsing feed", e);
 				activity.goBlooey(e);
@@ -51,11 +61,83 @@ public class FeedActivity extends ListActivity {
 		}
 	}
 	
+	private class FeedAdapter extends BaseAdapter{
+		RSSFeed feed;
+		
+		FeedAdapter(RSSFeed feed){
+			super();
+			this.feed = feed;
+		}
+		
+		public int getCount(){
+			return feed.getItems().size();
+		}
+		
+		public Object getItem(int position){
+			return feed.getItems().get(position);
+		}
+		
+		public long getItemId(int position){
+			return position;
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent){
+			View row = convertView;
+			if(row == null) {
+				LayoutInflater inflater = getLayoutInflater();
+				
+				row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+			
+			RSSItem item = (RSSItem)getItem(position);
+			((TextView)row).setText(item.getTitle());
+			
+			return row;
+		}
+	}
+	
+	private static class InstanceState {
+		RSSFeed feed;
+		FeedTask task;
+	}
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		state = (InstanceState)getLastNonConfigurationInstance();
+		
+		if(state == null){
+			state = new InstanceState();
+			state.task = new FeedTask(this);
+			state.task.execute(getIntent().getStringExtra(FEED_URL));
+			
+		} else {
+			if (state.task != null) { 
+				state.task.attach(this);
+			}
+			if (state.feed != null) { 
+				setFeed(state.feed);
+			}
+		}
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance(){
+		if(state.task != null){
+			state.task.detach();
+		}
+		return state;
+	}
+	
+	private void setFeed(RSSFeed feed){
+		state.feed = feed;
+		setListAdapter(new FeedAdapter(feed));
+	}
+	
+	
 	private void goBlooey(Throwable t) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Exception!").setMessage(t.toString()).setPositiveButton("OK", null).show();
 	}
-	
-	
 	
 }
