f6fbe56a31eb1b3e8145a248e90ff70bbd7786ef
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-10-09 15:06:03 -0600

    Added the RSSReader load logic to onHandleIntent()

--- LunchList/src/apt/tutorial/FeedService.java
+++ LunchList/src/apt/tutorial/FeedService.java
@@ -1,5 +1,28 @@
 package apt.tutorial;
 
-public class FeedService {
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
 
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class FeedService extends IntentService {
+	
+	public static final String EXTRA_URL = "apt.tutorial.EXTRA_URL";
+	
+	public FeedService() {
+		super("FeedService");
+	}
+	
+	@Override
+	public void onHandleIntent(Intent i) {
+		RSSReader reader = new RSSReader();
+		
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		} catch (Exception e) {
+			Log.e("LunchList", "Exeption parsing feed", e);
+		}
+	}
 }
