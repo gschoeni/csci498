f63f700d36ad0913d5655cd2b085834de82c4d94
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-10-21 19:54:40 -0600

    added option in option menu for map

--- LunchList/gen/apt/tutorial/R.java
+++ LunchList/gen/apt/tutorial/R.java
@@ -23,23 +23,25 @@ public final class R {
         public static final int ic_menu_add=0x7f020005;
         public static final int ic_menu_compass=0x7f020006;
         public static final int ic_menu_friendslist=0x7f020007;
-        public static final int ic_menu_preferences=0x7f020008;
-        public static final int list=0x7f020009;
-        public static final int restaurant=0x7f02000a;
-        public static final int run=0x7f02000b;
-        public static final int toast=0x7f02000c;
+        public static final int ic_menu_mapmode=0x7f020008;
+        public static final int ic_menu_preferences=0x7f020009;
+        public static final int list=0x7f02000a;
+        public static final int restaurant=0x7f02000b;
+        public static final int run=0x7f02000c;
+        public static final int toast=0x7f02000d;
     }
     public static final class id {
-        public static final int add=0x7f09000c;
+        public static final int add=0x7f09000d;
         public static final int address=0x7f090001;
         public static final int delivery=0x7f090005;
         public static final int feed=0x7f090008;
         public static final int icon=0x7f090009;
         public static final int location=0x7f090006;
+        public static final int map=0x7f09000c;
         public static final int menu_settings=0x7f09000b;
         public static final int name=0x7f090000;
         public static final int notes=0x7f090007;
-        public static final int prefs=0x7f09000d;
+        public static final int prefs=0x7f09000e;
         public static final int sit_down=0x7f090004;
         public static final int take_out=0x7f090003;
         public static final int title=0x7f09000a;
