202120f32e1845761b70e8bfa6dfd36d66cc2b25
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-10-26 12:03:27 -0600

    stubbed out a BootReceiver and added a receiver to the AndroidManifest

---
+++ LunchList/src/apt/tutorial/OnBootReciever.java
@@ -0,0 +1,14 @@
+package apt.tutorial;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReciever extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context ctxt, Intent intent) {
+		
+	}
+
+}
