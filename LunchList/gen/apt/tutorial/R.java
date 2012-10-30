cdedcf6806bff1c95dc4cbc7d9d3d778c76b678a
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-10-30 14:06:28 -0600

    added images, and xml files, and bare class for app widget

--- LunchList/gen/apt/tutorial/R.java
+++ LunchList/gen/apt/tutorial/R.java
@@ -18,32 +18,36 @@ public final class R {
         public static final int ball_green=0x7f020000;
         public static final int ball_red=0x7f020001;
         public static final int ball_yellow=0x7f020002;
-        public static final int ic_action_search=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int ic_menu_add=0x7f020005;
-        public static final int ic_menu_compass=0x7f020006;
-        public static final int ic_menu_friendslist=0x7f020007;
-        public static final int ic_menu_mapmode=0x7f020008;
-        public static final int ic_menu_preferences=0x7f020009;
-        public static final int list=0x7f02000a;
-        public static final int map_pin=0x7f02000b;
-        public static final int restaurant=0x7f02000c;
-        public static final int run=0x7f02000d;
-        public static final int stat_notify_chat=0x7f02000e;
-        public static final int toast=0x7f02000f;
+        public static final int ff=0x7f020003;
+        public static final int ic_action_search=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int ic_menu_add=0x7f020006;
+        public static final int ic_menu_compass=0x7f020007;
+        public static final int ic_menu_friendslist=0x7f020008;
+        public static final int ic_menu_mapmode=0x7f020009;
+        public static final int ic_menu_preferences=0x7f02000a;
+        public static final int icon=0x7f02000b;
+        public static final int list=0x7f02000c;
+        public static final int map_pin=0x7f02000d;
+        public static final int restaurant=0x7f02000e;
+        public static final int run=0x7f02000f;
+        public static final int stat_notify_chat=0x7f020010;
+        public static final int toast=0x7f020011;
+        public static final int widget_frame=0x7f020012;
     }
     public static final class id {
-        public static final int add=0x7f09000d;
+        public static final int add=0x7f09000e;
         public static final int address=0x7f090001;
         public static final int delivery=0x7f090005;
         public static final int feed=0x7f090008;
         public static final int icon=0x7f09000a;
         public static final int location=0x7f090006;
         public static final int map=0x7f090009;
-        public static final int menu_settings=0x7f09000c;
+        public static final int menu_settings=0x7f09000d;
         public static final int name=0x7f090000;
+        public static final int next=0x7f09000c;
         public static final int notes=0x7f090007;
-        public static final int prefs=0x7f09000e;
+        public static final int prefs=0x7f09000f;
         public static final int sit_down=0x7f090004;
         public static final int take_out=0x7f090003;
         public static final int title=0x7f09000b;
@@ -55,6 +59,7 @@ public final class R {
         public static final int detail_form=0x7f030002;
         public static final int map=0x7f030003;
         public static final int row=0x7f030004;
+        public static final int widget=0x7f030005;
     }
     public static final class menu {
         public static final int activity_lunch_list=0x7f080000;
@@ -83,5 +88,6 @@ public final class R {
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
+        public static final int widget_provider=0x7f040001;
     }
 }
---
+++ LunchList/src/apt/tutorial/AppWidget.java
@@ -0,0 +1,7 @@
+package apt.tutorial;
+
+import android.appwidget.AppWidgetProvider;
+
+public class AppWidget extends AppWidgetProvider {
+	
+}
