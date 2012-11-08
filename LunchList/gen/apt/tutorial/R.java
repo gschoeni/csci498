4bffae1b63480bd19eeb0c94a0525c64f71447ed
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-11-07 22:24:50 -0700

    added new widget layout and xml

--- LunchList/gen/apt/tutorial/R.java
+++ LunchList/gen/apt/tutorial/R.java
@@ -36,18 +36,19 @@ public final class R {
         public static final int widget_frame=0x7f020012;
     }
     public static final class id {
-        public static final int add=0x7f09000e;
+        public static final int add=0x7f09000f;
         public static final int address=0x7f090001;
         public static final int delivery=0x7f090005;
         public static final int feed=0x7f090008;
         public static final int icon=0x7f09000a;
         public static final int location=0x7f090006;
         public static final int map=0x7f090009;
-        public static final int menu_settings=0x7f09000d;
+        public static final int menu_settings=0x7f09000e;
         public static final int name=0x7f090000;
         public static final int next=0x7f09000c;
         public static final int notes=0x7f090007;
-        public static final int prefs=0x7f09000f;
+        public static final int prefs=0x7f090010;
+        public static final int restaurants=0x7f09000d;
         public static final int sit_down=0x7f090004;
         public static final int take_out=0x7f090003;
         public static final int title=0x7f09000b;
