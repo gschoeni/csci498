772e89ab9099a5dfca7adbf64369f01933c3f1de
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-11-13 17:46:20 -0700

    removed the squirrel from in here, he is in a better place now

--- Squirrel Scamper/gen/com/gregschoeninger/SquirrelScamper/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.gregschoeninger.SquirrelScamper;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
