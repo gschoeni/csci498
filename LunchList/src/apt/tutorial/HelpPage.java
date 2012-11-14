fec6f9a669375cfcc168ab4a26d6b2aed9558a26
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-11-13 19:19:43 -0700

    added the HelpPage class and added the activity to the manifest

---
+++ LunchList/src/apt/tutorial/HelpPage.java
@@ -0,0 +1,21 @@
+package apt.tutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class HelpPage extends Activity {
+	
+	private WebView browser;
+	
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.help);
+		
+		browser = (WebView)findViewById(R.id.webkit);
+		browser.loadUrl("file:///android_asset/help.html");
+	}
+	
+	
+}
